0f3a8fc78c8f7d3349ad7184b139e30164e4a0c4
Tai Nguyen <tainnguyen2@gmail.com>
2026-02-07 12:44:11 -0800

    Initial Commit

---
+++ ikid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ikid
+//
+//  Created by Tai Nguyen on 2/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
